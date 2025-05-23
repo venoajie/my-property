d158e0228f7c85cf00d4478b1745ecd871431c94
venoajie <59521823+venoajie@users.noreply.github.com>
2025-05-24 05:03:00 +0700

    add config template related files

--- real_estate/settings/production.py
+++ real_estate/settings/production.py
@@ -7,6 +7,13 @@ DEBUG = TRUE
 ALLOWED_HOSTS = ['130.61.246.120', 'localhost', '127.0.0.1']  # Temporary for debugging
 ALLOWED_HOSTS = ['*']  # Temporary for debugging
 
+MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+] + MIDDLEWARE
+
+# Configure static files storage
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
