43e8d772fe17f901c8d5700133bea78b821459a5
venoajie <59521823+venoajie@users.noreply.github.com>
2025-05-23 22:06:37 +0700

    Update production.py

--- real_estate/settings/production.py
+++ real_estate/settings/production.py
@@ -3,7 +3,8 @@ from .base import *
 DEBUG = False
 
 # Add these lines
-[ALLOWED_HOSTS = ['141.147.54.12', 'localhost', '127.0.0.1']]  # Temporary for debugging
+ALLOWED_HOSTS = ['141.147.54.12', 'localhost', '127.0.0.1']  # Temporary for debugging
+
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
