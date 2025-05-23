ec8dd5fdd2406e1bcf4bacd9b25e81280c31370c
venoajie <59521823+venoajie@users.noreply.github.com>
2025-05-23 21:05:57 +0700

    prepare initial files

---
+++ real_estate/settings/production.py
@@ -0,0 +1,19 @@
+from .base import *
+
+DEBUG = False
+
+# Add these lines
+ALLOWED_HOSTS = ['*']  # Temporary for debugging
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'WARNING',
+    },
+}
