74ce483b7623772532f929736b2809ca2cc38837
venoajie <59521823+venoajie@users.noreply.github.com>
2025-05-24 04:44:45 +0700

    test app

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,11 @@
 from django.contrib import admin
 from django.urls import path
 
+def home(request):
+    from django.http import HttpResponse
+    return HttpResponse("Django is working!")
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home)
 ]
