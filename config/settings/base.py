82a5e7323f9e1da90f3a0a57843edca9d068589e
venoajie <59521823+venoajie@users.noreply.github.com>
2025-05-24 07:15:52 +0700

    Update base.py

--- config/settings/base.py
+++ config/settings/base.py
@@ -31,7 +31,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "listings",  # Custom app
+    "apps.listings",  # Custom app
 ]
 
 # ----- Middleware -----
