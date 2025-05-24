fa2c94c2f29322dbc6026101011c9b123d6739ef
venoajie <59521823+venoajie@users.noreply.github.com>
2025-05-24 19:27:47 +0700

    Update urls.py

---
+++ apps/users/urls.py
@@ -0,0 +1,11 @@
+# apps/users/urls.py
+from django.urls import path
+from . import views
+
+app_name = "users"
+
+urlpatterns = [
+    # path("login/", views.LoginView.as_view(), name="login"),
+    # path("register/", views.RegisterView.as_view(), name="register"),
+    # Add other user-related endpoints here
+]
