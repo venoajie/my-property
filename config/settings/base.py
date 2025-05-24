39321b77263ebd51bbea75c5c9bba2d859254976
venoajie <59521823+venoajie@users.noreply.github.com>
2025-05-24 09:13:11 +0700

    update tests related file

--- config/settings/base.py
+++ config/settings/base.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "apps.listings",  # Custom app
+    "core",
+    "listings",  
+    "users"",
 ]
 
 # ----- Middleware -----
